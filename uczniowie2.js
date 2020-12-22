4954eeee858bb267bd362b206725af8f34eb3e61
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-22 12:26:47 +0100

    punkt wyjścia - zadanie 5

---
+++ uczniowie2.js
@@ -0,0 +1,104 @@
+var uczniowie2 = [
+    {
+      "imie": "Artur",
+      "nazwisko": "T.",
+      "oceny": "6 2 3 3 6 5 2"
+    },
+    {
+      "imie": "Marta",
+      "nazwisko": "B.",
+      "oceny": "5 4 5 3 3 1 2"
+    },
+    {
+      "imie": "Monika",
+      "nazwisko": "U.",
+      "oceny": "2 4 2 6 6 6 5"
+    },
+    {
+      "imie": "Amelia",
+      "nazwisko": "L.",
+      "oceny": "4 3 4 3 4 3 4"
+    },
+    {
+      "imie": "Marta",
+      "nazwisko": "K.",
+      "oceny": "5 5 5 2 4 2 5"
+    },
+    {
+      "imie": "Andrzej",
+      "nazwisko": "P.",
+      "oceny": "3 3 3 4 2 3 3"
+    },
+    {
+      "imie": "Andrzej",
+      "nazwisko": "H.",
+      "oceny": "2 5 2 1 2 1 5"
+    },
+    {
+      "imie": "Paweł",
+      "nazwisko": "Q.",
+      "oceny": "1 2 1 2 1 3 3"
+    },
+    {
+      "imie": "Katarzyna",
+      "nazwisko": "T.",
+      "oceny": "2 2 1 4 4 4 1"
+    },
+    {
+      "imie": "Andrzej",
+      "nazwisko": "Q.",
+      "oceny": "1 3 4 3 3 1 3"
+    },
+    {
+      "imie": "Andrzej",
+      "nazwisko": "O.",
+      "oceny": "3 6 3 4 6 5 5"
+    },
+    {
+      "imie": "Amelia",
+      "nazwisko": "I.",
+      "oceny": "5 5 5 3 4 5 5"
+    },
+    {
+      "imie": "Amelia",
+      "nazwisko": "V.",
+      "oceny": "4 5 1 2 1 2 2"
+    },
+    {
+      "imie": "Kacper",
+      "nazwisko": "Z.",
+      "oceny": "5 3 3 2 2 3 4"
+    },
+    {
+      "imie": "Monika",
+      "nazwisko": "R.",
+      "oceny": "6 2 5 3 3 6 4"
+    },
+    {
+      "imie": "Miriam",
+      "nazwisko": "E.",
+      "oceny": "3 3 2 2 3 4 2"
+    },
+    {
+      "imie": "Monika",
+      "nazwisko": "N.",
+      "oceny": "3 3 3 5 3 4 3"
+    },
+    {
+      "imie": "Michał",
+      "nazwisko": "U.",
+      "oceny": "5 5 3 4 5 3 3"
+    },
+    {
+      "imie": "Andrzej",
+      "nazwisko": "T.",
+      "oceny": "3 5 5 5 3 3 4"
+    },
+    {
+      "imie": "Marta",
+      "nazwisko": "W.",
+      "oceny": "5 3 3 4 5 3 5"
+    }
+  ]
+
+  
