86c689db56127970ac8b3a0264712d5e7663b97a
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-23 14:38:03 +0100

    namiastka gry

--- node-serwer/public/rysowanie01.js
+++ node-serwer/public/rysowanie01.js
@@ -1,12 +1,25 @@
 function rysuj(){
     var canv = document.querySelector("#cnv")
-    console.log(canv)
     var ctx = canv.getContext("2d")
-    ctx.lineWidth = 20
-    ctx.strokeStyle = "#181"
-    ctx.moveTo( 0, 0)
-    ctx.lineTo( 100, 100 )
+
+    rysuj_bazy( ctx, Gra.bazy )
+
+}
+
+function rysuj_baze( ctx, baza ){
+    const rozm = 10
+    ctx.rect( baza.x - rozm/2, baza.y - rozm/2, rozm, rozm );
+    ctx.fillStyle = "#00ff00"
+    ctx.fill()
+    ctx.lineWidth = 2
+    ctx.strokeStyle = "#111"
     ctx.stroke()
+}
+
+function rysuj_bazy( ctx, bazy ){
+    for ( const baza of bazy ){
+        rysuj_baze(ctx, baza)
+    }
 }
 
 window.onload = rysuj
